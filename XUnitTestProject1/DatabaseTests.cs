b03c62affbb01f79337e9308d06965a99d51a03c
Hakan Tuncer <hakantuncer@gmail.com>
2020-06-06 15:18:13 +0200

    move to ef core 3.1 and try to reproduce the issue

--- XUnitTestProject1/DatabaseTests.cs
+++ XUnitTestProject1/DatabaseTests.cs
@@ -8,47 +8,48 @@ namespace EfCoreBugExperiment
     public abstract class DatabaseTests
     {
         protected DbContextOptions<MyDbContext> ContextOptions;
+        private readonly string _addressId = Guid.NewGuid().ToString();
 
         protected DatabaseTests(DbContextOptions<MyDbContext> contextOptions)
         {
             ContextOptions = contextOptions;
-
             Seed();
         }
 
         private void Seed()
         {
-            using (var context = new MyDbContext(ContextOptions))
+            using var context = new MyDbContext(ContextOptions);
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            context.Addresses.Add(new Address()
             {
-                context.Database.EnsureDeleted();
-                context.Database.EnsureCreated();
-            }
+                Id = _addressId,
+                Street = "Damrak",
+                PostCode = "1000AB",
+                City = "Amsterdam",
+            });
+
+            context.SaveChanges();
         }
 
         [Fact]
         public async Task Adding_Related_Entity_OnTheFly_Works()
         {
             // Act
-            using (var context = new MyDbContext(ContextOptions))
+            await using (var context = new MyDbContext(ContextOptions))
             {
-                await context.Addresses.AddAsync(new Address()
+                var address = await context.Addresses.FindAsync(_addressId);
+                address.Person = new Person
                 {
                     Id = Guid.NewGuid().ToString(),
-                    Street = "Damrak",
-                    PostCode = "1000AB",
-                    City = "Amsterdam",
-                    Person = new Person
-                    {
-                        Id = Guid.NewGuid().ToString(),
-                        Name = "Hakan Tuncer"
-                    }
-                });
-
+                    Name = "Hakan Tuncer"
+                };
                 await context.SaveChangesAsync();
             }
 
             // Assert
-            using (var context = new MyDbContext(ContextOptions))
+            await using (var context = new MyDbContext(ContextOptions))
             {
                 var personCount = await context.Persons.CountAsync();
                 var addressCount = await context.Addresses.CountAsync();
